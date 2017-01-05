57468253157e919ca90a1d38276effc2749d275d
Zeyuan Shang <zeyuanxy@gmail.com>
2017-01-06 03:14:38 +0800

    [p1] mtest is passed. P1 is finished!

--- p1/src/github.com/cmu440/bitcoin/miner/miner.go
+++ p1/src/github.com/cmu440/bitcoin/miner/miner.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/cmu440/bitcoin"
@@ -12,8 +11,7 @@ import (
 // Attempt to connect miner as a client to the server.
 func joinWithServer(hostport string) (lsp.Client, error) {
 	client, err := lsp.NewClient(hostport, lsp.NewParams())
-	joinMessage := &bitcoin.QueryWithMessage{0, *bitcoin.NewJoin()}
-	err = bitcoin.SendMessage(client, joinMessage)
+	err = bitcoin.SendMessage(client, *bitcoin.NewJoin())
 	if err != nil {
 		return nil, err
 	}
@@ -21,7 +19,7 @@ func joinWithServer(hostport string) (lsp.Client, error) {
 	return client, nil
 }
 
-func workOnJob(client lsp.Client, queryId int, data string, lower uint64, upper uint64) {
+func workOnJob(client lsp.Client, data string, lower uint64, upper uint64) {
 	nonce, minHash := lower, bitcoin.Hash(data, lower)
 	for i := lower + 1; i <= upper; i++ {
 		hash := bitcoin.Hash(data, i)
@@ -34,9 +32,8 @@ func workOnJob(client lsp.Client, queryId int, data string, lower uint64, upper 
 	resultMessage := bitcoin.NewResult(minHash, nonce)
 	resultMessage.Lower = lower
 	resultMessage.Upper = upper
-	resultQueryWithMessage := &bitcoin.QueryWithMessage{queryId, *resultMessage}
 
-	err := bitcoin.SendMessage(client, resultQueryWithMessage)
+	err := bitcoin.SendMessage(client, resultMessage)
 	if err != nil {
 		fmt.Printf("[Miner %v] Error: %v\n", client.ConnID(), err)
 	} else {
@@ -45,20 +42,6 @@ func workOnJob(client lsp.Client, queryId int, data string, lower uint64, upper 
 }
 
 func main() {
-	const (
-		name = "log_miner.txt"
-		flag = os.O_RDWR | os.O_CREATE
-		perm = os.FileMode(0666)
-	)
-
-	file, err := os.OpenFile(name, flag, perm)
-	if err != nil {
-		return
-	}
-	defer file.Close()
-
-	LOGF := log.New(file, "", log.Lshortfile|log.Lmicroseconds)
-
 	const numArgs = 2
 	if len(os.Args) != numArgs {
 		fmt.Printf("Usage: ./%s <hostport>", os.Args[0])
@@ -77,15 +60,13 @@ func main() {
 	defer miner.Close()
 
 	for {
-		queryWithMessage, err := bitcoin.GetMessage(miner)
+		message, err := bitcoin.GetMessage(miner)
 		if err == nil {
-			queryId, message := queryWithMessage.QueryId, queryWithMessage.Message
-
 			switch message.Type {
 			case bitcoin.Request:
 				data, lower, upper := message.Data, message.Lower, message.Upper
 				LOGF.Printf("Get Request from Server: %v, %v, %v\n", data, lower, upper)
-				go workOnJob(miner, queryId, data, lower, upper)
+				go workOnJob(miner, data, lower, upper)
 			default:
 			}
 		} else {
